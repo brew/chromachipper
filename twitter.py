731834bc13cbd55ed6e1c0c011822bbf633ecc72
Brook Elgie <specialbrew@gmail.com>
2022-02-26 16:49:30 +0000

    Pass in twitter_id

--- twitter.py
+++ twitter.py
@@ -92,7 +92,7 @@ class ChromachipStreamListener(tweepy.StreamListener):
 
 if __name__ == '__main__':
     app_id = os.environ.get('APP_ID')
-    twitter_id = int(os.environ.get('TWITTER_ID'))
+    twitter_id = os.environ.get('TWITTER_ID')
     consumer_token = os.environ.get('CONSUMER_TOKEN')
     consumer_secret = os.environ.get('CONSUMER_SECRET')
     access_token = os.environ.get('ACCESS_TOKEN')
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
-    listener = ChromachipStreamListener(api, twitter_id)
+    listener = ChromachipStreamListener(api, int(twitter_id))
 
     stream = tweepy.Stream(auth, listener)
     stream.filter(follow=[twitter_id], is_async=True)
